e04463884d8b68d15a33e21331a606b26f110ed8
Frank Schmidt <rubiccuber@googlemail.com>
2016-01-08 18:08:10 +0100

    install now sets up i3 configs

--- install.sh
+++ install.sh
@@ -5,15 +5,19 @@ HOME=$(cd;pwd)
 DOTFILES=$(pwd)
 echo "home $HOME"
 echo "dotfiles $DOTFILES"
+echo "'''''''''''"
 
 function link_home {
 	if [[ -e $HOME/$2 ]]; then
-		echo [W] file $2 exists, not linking
+		echo [N] file \$HOME/$2 exists, not linking
 	else
-		echo do stuff
-		echo $DOTFILES/$1 $HOME/$2
+		echo "linking $1 -> $HOME/$2"
+		ln -s $DOTFILES/$1 $HOME/$2
 	fi
 }
 
 link_home zshrc .zshrc
 link_home vimrc .vimrc
+mkdir -p $HOME/.i3
+link_home i3-config .i3/config
+link_home i3status.conf .i3status.conf

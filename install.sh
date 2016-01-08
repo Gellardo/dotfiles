e1bc024c407a37cda8bae84dbc60aa3cc69bf1ce
Frank Schmidt <rubiccuber@googlemail.com>
2016-01-08 21:59:47 +0100

    made install file more robust
    no output on already correctly linked files
    if called from another dir, changes to correct one

--- install.sh
+++ install.sh
@@ -2,22 +2,29 @@
 # link config files to the correct places
 
 HOME=$(cd;pwd)
-DOTFILES=$(pwd)
+# change to dotfiles dir and save dirname to variable
+DOTFILES="$( cd "$( dirname "$0" )" && pwd )"
 echo "home $HOME"
 echo "dotfiles $DOTFILES"
-echo "'''''''''''"
+echo "-----------"
 
 function link_home {
 	if [[ -e $HOME/$2 ]]; then
-		echo [N] file \$HOME/$2 exists, not linking
+		if [[ $(readlink $HOME/$2) != "$DOTFILES/$1" ]]; then
+			echo "[W] file \$HOME/$2 exists, not linking"
+		fi
 	else
 		echo "linking $1 -> $HOME/$2"
 		ln -s $DOTFILES/$1 $HOME/$2
 	fi
 }
 
+echo "Start linking files"
+
 link_home zshrc .zshrc
 link_home vimrc .vimrc
 mkdir -p $HOME/.i3
 link_home i3-config .i3/config
 link_home i3status.conf .i3status.conf
+
+echo "Successfully done"
